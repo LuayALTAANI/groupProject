bee07c2e89cd77d2e59b32043d487cb92e884529
Luay <121561226+LuayALTAANI@users.noreply.github.com>
2025-07-02 20:13:06 -0400

    insert information into an HTML table

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-let output = document.getElementById("output");
+const employeeName = document.querySelector("#fullname");
+const role = document.querySelector("#role");
+const YearsOfExperience = document.querySelector("#experience");
+const education = document.querySelector("#education");
+const skills = document.querySelector("#skills");
+const certifications = document.querySelector("#certification");
+const languagesSpoken = document.querySelector("#languages");
+const portfolioLink = document.querySelector("#portfolio");
+const WillingToRelocate = document.querySelector("#relocate");
+const remoteWorkPreferred = document.querySelector("#remote");
+const softSkills = document.querySelector("#softskills");
+const openToInternship = document.querySelector("#internship");
 
 function Person(name, location, targetRole, YearsOfExperience, education, skills, certifications, languagesSpoken, portfolioLink, WillingToRelocate, remoteWorkPreferred, softSkills, openToInternship) {
     this.name = name;
@@ -23,7 +34,7 @@ let priyaNair = new Person(
     3,
     "Bcs in Statistics, University of Toronto",
     ["Python", "SQL", "Tableau", "Excel"],
-    ["GOOGLE data analytics", "IBM data analytics"],
+    ["Google data analytics", "IBM data analytics"],
     ["English", "Hindi", "Tamil"],
     "https://priyanairdata.ca",
     false,
@@ -31,3 +42,16 @@ let priyaNair = new Person(
     ["Detail-oriented", "Analytical thinking"],
     false
 );
+
+employeeName.textContent = priyaNair.name;
+role.textContent = priyaNair.targetRole;
+YearsOfExperience.textContent = priyaNair.YearsOfExperience;
+education.textContent = priyaNair.education;
+skills.textContent = priyaNair.skills.join(", ");
+certifications.textContent = priyaNair.certifications.join(", ");
+languagesSpoken.textContent = priyaNair.languagesSpoken.join(", ");
+portfolioLink.innerHTML = `<a href="${priyaNair.portfolioLink}">${priyaNair.portfolioLink}</a>`;
+WillingToRelocate.textContent = priyaNair.WillingToRelocate ? "Yes" : "No";
+remoteWorkPreferred.textContent = priyaNair.remoteWorkPreferred ? "Yes" : "No";
+softSkills.textContent = priyaNair.softSkills.join(", ");
+openToInternship.textContent = priyaNair.openToInternship ? "Yes" : "No";
